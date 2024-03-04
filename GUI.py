32b0a1f08c530010d2379dd381084f05f00b79bf
Carlos Dutra <carlosdutra@usp.br>
2024-03-04 13:53:07 +0100

    first commit

---
+++ GUI.py
@@ -0,0 +1,12 @@
+#import tifffile as tiff
+from PIL import Image, ImageDraw
+import numpy as np 
+from tkinter import *
+
+# Interface definition
+class Application:
+    def __init__(self, master=None):
+        pass
+root = Tk()
+Application(root)
+root.mainloop()

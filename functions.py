eeb81983d64afc9e86d3e756122fdc7654050179
MarcGlz <cram.gleyze@gmail.com>
2024-03-05 16:16:15 +0100

    05/03

--- functions.py
+++ functions.py
@@ -1,22 +1,27 @@
 import numpy as np
 
-def contrast(image, valeur_contraste):
-    #Calculate the contrasted image by multiplying by the contrast value
-    image_contrastee = np.clip(image * valeur_contraste, 0, 255).astype(np.uint8)
+def contrast(images, factor):
+    # Adjust contrast with linear transform
+    mean_pixel_value = np.mean(images)
     
-    return image_contrastee
+    adjusted_images = (images - mean_pixel_value) * factor + mean_pixel_value
+    adjusted_images = np.clip(adjusted_images, 0, 2**16-1).astype(np.uint16)
+
+    return adjusted_images
 
 
 def brightness(image, valeur_luminosite):
     # Calculate the image with adjusted brightness by adding the brightness value
-    image_lumineuse = np.clip(image + valeur_luminosite, 0, 255).astype(np.uint8)
+    image_lumineuse = np.clip(image + valeur_luminosite, 0, 2**16-1).astype(np.uint16)
     
     return image_lumineuse
 
-def moyennage_temporel(images, nb_images):
-    #Calculate the temporal averages with a window of number of nb_images
+def moyennage_temporel(images, window):
+    #Calculate the temporal averages with a window 
     n,x,y = images.shape
-    for i in range(n-nb_images+1):
-        images[i] = np.mean(images[i:i+20,:,:], axis = 0)
-    return images
-    
+    for i in range(n-window+1):
+        images[i] = np.mean(images[i:i+window,:,:], axis = 0)
+    return images  
+
+def invert_colors(images):
+    return   2**16 - images

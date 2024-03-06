4c7dee81eed568cc4796e38012a6a6f29758b3b3
caique <carlosdutra87@gmail.com>
2024-03-06 15:49:32 +0100

    Load project method included. Load screen 100% functional.

--- GUI.py
+++ GUI.py
@@ -5,11 +5,9 @@ import tkinter as tk
 from tkinter import filedialog
 from image_viewer import ImageViewer
 from data_viewer import DataViewer  
-#from graph_viewer import GraphViewer
-from graph_viewer import ImageSliderApp
+from graph_viewer import GraphViewer
 from file_viewer import FileViewer
 from load_screen import LoadScreen
-#from ttkthemes import ThemedStyle
 
 class GUI(tk.Toplevel):
     def __init__(self, master=None, app=None, image_path=None):
@@ -59,9 +57,8 @@ class GUI(tk.Toplevel):
         data_graph_frame.add(self.data_viewer, minsize=100)  # Set minimum size
 
         # Graph Viewer
-        #self.graph_viewer = GraphViewer(data_graph_frame)
-        self.image_slider = ImageSliderApp(data_graph_frame)
-        data_graph_frame.add(self.image_slider, minsize=100)  # Set minimum size
+        self.graph_viewer = GraphViewer(data_graph_frame)
+        data_graph_frame.add(self.graph_viewer, minsize=100)  # Set minimum size
         
         self.load_screen = LoadScreen(self.master, app=self, image_viewer=self.image_viewer)
 
